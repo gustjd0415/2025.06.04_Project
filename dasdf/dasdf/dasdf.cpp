7261651ef469c8f5b7f5c926fc05c46df693fbfa
gustjd0415 <gustjd0415_@naver.com>
2025-06-05 14:51:40 +0900

    1번 과제 수정

--- dasdf/dasdf/dasdf.cpp
+++ dasdf/dasdf/dasdf.cpp
@@ -6,26 +6,26 @@ int main()
 {
     double score[5];
 
-    cout << "1 번째 학생 점수를 입력해주세요 : ";
+    cout << "1 : ";
     cin >> score[0];
 
-    cout << "2 번째 학생 점수를 입력해주세요 : ";
+    cout << "2 : ";
     cin >> score[1];
 
-    cout << "3 번째 학생 점수를 입력해주세요 : ";
+    cout << "3 : ";
     cin >> score[2];
 
-    cout << "4 번째 학생 점수를 입력해주세요 : ";
+    cout << "4 : ";
     cin >> score[3];
 
-    cout << "5 번째 학생 점수를 입력해주세요 : ";
+    cout << "5 : ";
     cin >> score[4];
 
     double total = score[0] + score[1] + score[2] + score[3] + score[4];
     double average = total / 5;
 
-    cout << "총점 : " << total << endl;
-    cout << "평균 : " << average << endl;
+    cout << "Total : " << total << endl;
+    cout << "Average : " << average << endl;
 
     return 0;
 }
